ccbd7ef637596a6479919ae12ddbd30323152828
Danyelss <61598630+Danyelss@users.noreply.github.com>
2021-01-07 20:22:40 +0200

    Connected to Sandbox

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React from "react";
 import "./styles.css";
-import { BrowserRouter as Router,
+import {
+  BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
-   } from  "react-router-dom";
+  Redirect
+} from "react-router-dom";
 
-import FirstPage from './pages';
-import NotFoundPage from './pages/404';
-import Lista from './pages/ListaCarti';
-import CartePreferata from './pages/';
-import UltimaCitita from './pages/';
+import FirstPage from "./pages";
+import NotFoundPage from "./pages/404";
+import Lista from "./pages/ListaCarti";
+import CartePreferata from "./pages/";
+import UltimaCititae from "./pages/";
 
-const App = () =>{
-  return(
-      <div>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={FirstPage}></Route>
-            <Route exact path="/404" component={NotFoundPage}></Route>
-            <Route exact path="/lista" component={Lista}></Route>
-            <Route exact path="/" component={CartePreferata}></Route>
-            <Route exact path="/" component={UltimaCitita}></Route>
-            <Redirect to="/404"></Redirect>
-          </Switch>
-        </Router>
-      </div>
-  )
-}
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={FirstPage}></Route>
+          <Route exact path="/404" component={NotFoundPage}></Route>
+          <Route exact path="/lista" component={Lista}></Route>
+          <Route exact path="/" component={CartePreferata}></Route>
+          <Route exact path="/" component={UltimaCititae}></Route>
+          <Redirect to="/404"></Redirect>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 export default App;
